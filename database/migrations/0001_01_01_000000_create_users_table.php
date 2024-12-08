b33cd4b87296f98888f5e25047810836cc651263
Brayan Escobar <brayand.escobar@autonoma.edu.co>
2024-12-07 22:13:04 -0500

    Revert "CRUD Users"

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,18 +12,29 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->string('id')->unique();
+            $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('address');
-            $table->string('gender');
-            $table->integer('age');
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('health_history');
-            $table->enum('user_type', ['admin', 'user', 'doctor']);
+            $table->rememberToken();
             $table->timestamps();
         });
 
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
     }
 
     /**
@@ -32,5 +43,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
     }
 };
--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController;
-
-Route::apiResource('users', UserController::class);
-
-
